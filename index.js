00063d52c17ecfbbc931cff58631f7cbfd23d7d5
JenistanT26 <josephjenistan1@gmail.com>
2024-10-14 19:26:23 +0530

    app structure

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app=express();
+
+const port=4000;
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
